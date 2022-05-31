35c0d4c419cbe97548bf4915477e819c2b659176
Aayush <adeshara@aayush.bbrouter>
2022-05-31 16:57:40 +0530

    fix typing feature

--- app/channels/typing_channel.rb
+++ app/channels/typing_channel.rb
@@ -9,11 +9,15 @@ class TypingChannel < ApplicationCable::Channel
 
   def isTyping(data)
     # broadcast_to "typing_channel", partial: "users/status", user: self
+    typingUser = User.find(data["user"].to_i)
     if data["is_typing"]
-      ActionCable.server.broadcast "typing_channel", "<turbo-stream action=\"replace\" target=\"typing_#{data["connection_id"]}\"><template><turbo-frame id=\"typing_channel\"></turbo-frame>\n<div id=\"typing_#{data["connection_id"]}\"><div class=\"loader\"><span></span>
-      <span></span><span></span></div></div>\n</template></turbo-stream>"
+      ActionCable.server.broadcast "typing_channel", "<turbo-stream action=\"replace\" target=\"typing_#{data["connection_id"]}_#{data["user"]}\"><template><turbo-frame id=\"typing_channel\"></turbo-frame>\n<div id=\"typing_#{data["connection_id"]}_#{data["user"]}\"><div class=\"message-box\">
+      <div class=\"name dark-color font-message-author\"><b>#{typingUser.email}</b></div>
+      <div class=\"loader\"><span></span>
+      <span></span><span></span></div>
+  </div></div>\n</template></turbo-stream>"
     else
-      ActionCable.server.broadcast "typing_channel", "<turbo-stream action=\"replace\" target=\"typing_#{data["connection_id"]}\"><template><turbo-frame id=\"typing_channel\"></turbo-frame>\n<div id=\"typing_#{data["connection_id"]}\"></div>\n</template></turbo-stream>"
+      ActionCable.server.broadcast "typing_channel", "<turbo-stream action=\"replace\" target=\"typing_#{data["connection_id"]}_#{data["user"]}\"><template><turbo-frame id=\"typing_channel\"></turbo-frame>\n<div id=\"typing_#{data["connection_id"]}_#{data["user"]}\"></div>\n</template></turbo-stream>"
     end
   end
 
